93ae6ace63bfffe3f71e1d276765d64e6f941918
rishavroy97 <rishavroy008@gmail.com>
2019-11-13 10:17:57 +0530

    Add POST route for /playlist/create

--- routes/playlist.js
+++ routes/playlist.js
@@ -2,6 +2,43 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const passport = require('passport');
+const path = require('path');
+const multer = require('multer');
+const fs = require('fs-extra');
+const mongoose = require('mongoose');
+
+// Multer Setup
+
+const storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    let path = `./media/playlist/`;
+    fs.mkdirsSync(path);
+    cb(null, path);
+  },
+  filename: function(req, file, cb) {
+    cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+  }
+});
+
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
+    //accept a file
+    cb(null, true);
+  } else {
+    //reject a file
+    cb(null, false);
+  }
+};
+
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 1024 * 1024
+  },
+  fileFilter: fileFilter
+});
+
+
 // Load Playlist model
 const Playlist = require('../models/Playlist');
 const { ensureAuthenticated } = require('../config/auth');
@@ -13,22 +50,48 @@ router.get('/create', ensureAuthenticated, (req, res) =>
     subtitle: "Create Playlist"
 }));
 
-router.post('/create', (req, res) => {
-  const { title } = req.body;
+router.post('/create', ensureAuthenticated, upload.single("art"), (req, res) => {
+
   let errors = [];
 
+  const { title } = req.body;
   if (!title) {
     errors.push({ msg: 'Please enter all fields' });
-  }
-
-  if (errors.length > 0) {
-    res.render('', {
-      errors, title
+    fs.remove(req.file.path);
+    return res.render('createplaylist', {
+      errors, title,
+      user: req.user,
+      subtitle: "Create Playlist"
     });
-  } else {
-    // create playlist
   }
 
+  const album = new Playlist({
+    _id: new mongoose.Types.ObjectId(),
+    creator: req.user._id,
+    playlist_art: req.file.path,
+    title: req.body.title
+  });
+
+  return album
+    .save()
+    .then(result => {
+      req.flash(
+        'success_msg',
+        'Playlist created successfully'
+      );
+      return res.redirect('/dashboard');
+    }).catch(err => {
+      console.log(err);
+      errors.push({ msg: err });
+      if (errors.length > 0) {
+        console.log(errors)
+        return res.render('createplaylist', {
+          errors, title,
+          user: req.user,
+          subtitle: "Create Playlist"
+        });
+      }
+    })
 });
 
 // query methods
